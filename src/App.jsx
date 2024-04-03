9d03660f8ff144178a6ffafc036527efceb1cad3
Javlon <59303287+javlona@users.noreply.github.com>
2024-04-03 04:39:52 -0500

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Home from "./ui/Home";
 
 function App() {
+  createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
   return <></>;
 }
 
