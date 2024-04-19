745316f5603ee272d5e112e9a7d4b6abf345b050
Javlon <59303287+javlona@users.noreply.github.com>
2024-04-18 21:55:15 -0500

    some quick fixes

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,33 @@
 import { Form, redirect } from "react-router-dom";
 import { createOrder } from "../../services/apiFetch";
 
+const dummyCart = [
+  {
+    id: 11,
+    name: "Palov",
+    quantity: 1,
+    price: 9.99,
+    total: 9.99,
+  },
+  {
+    id: 12,
+    name: "Samsa",
+    quantity: 4,
+    price: 4.99,
+    total: 19.96,
+  },
+  {
+    id: 13,
+    name: "Kebab",
+    quantity: 2,
+    price: 14.99,
+    total: 29.98,
+  },
+];
+
 function CreateOrder() {
+  const cart = JSON.stringify(dummyCart);
+
   return (
     <div>
       <h2>Are you ready to order?</h2>
@@ -14,7 +40,7 @@ function CreateOrder() {
           <label htmlFor="phone">Phone number</label>
           <input type="tel" id="phone" name="phone" required />
           <div>
-            <input type="hidden" name="cart" value={JSON.stringify([])} />
+            <input type="hidden" name="cart" value={cart} />
             <button>Order</button>
           </div>
         </div>
